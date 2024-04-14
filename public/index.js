99d924e09e6a73609ce4508b85b61a952d3f2ad7
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-04-14 14:16:13 +0300

    1.1 fixed some bugs

--- public/index.js
+++ public/index.js
@@ -1,31 +1,25 @@
+const socket = io();
+
 document.addEventListener("DOMContentLoaded", function () {
+    const socket = io();
+
+    // Get references to the image and click count elements
     const cookieImg = document.getElementById("cookie-img");
     const clickCountElement = document.getElementById("click-count");
-    let clickCount = 0;
 
-    cookieImg.addEventListener("click", function () {
-        clickCount++;
+    // Listen for initial click count from server
+    socket.on("initialClickCount", (clickCount) => {
         clickCountElement.innerText = clickCount;
+    });
 
-        // Send click count to server
-        fetch("/update-click-count", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ clickCount: clickCount }),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Failed to update click count");
-                }
-                return response.text();
-            })
-            .then((data) => {
-                console.log(data); // Log server response
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+    // Listen for click event from server
+    socket.on("updateClickCount", (clickCount) => {
+        clickCountElement.innerText = clickCount;
+    });
+
+    // Add click event listener to the image
+    cookieImg.addEventListener("click", function () {
+        // Send click event to server
+        socket.emit("click");
     });
 });
