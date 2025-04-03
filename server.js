8403e37bb8d540ccd7e241786f5a6bdd95fee34a
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2025-04-03 14:55:37 +0300

    Add cookie persistence feature

--- server.js
+++ server.js
@@ -7,11 +7,35 @@ const io = require("socket.io")(http, {
         methods: ["GET", "POST"],
     },
 });
+const fs = require("fs");
+const path = require("path");
 
 app.use(express.static("./"));
 
 let playerCount = 0;
 let totalCookies = 0;
+
+// Load cookie count from file
+const cookieFile = path.join(__dirname, "cookies.dat");
+try {
+    if (fs.existsSync(cookieFile)) {
+        const data = fs.readFileSync(cookieFile, "utf8");
+        totalCookies = parseInt(data) || 0;
+        console.log(`Loaded ${totalCookies} cookies from file`);
+    }
+} catch (err) {
+    console.error("Error loading cookie count:", err);
+}
+
+// Function to save cookie count
+const saveCookieCount = () => {
+    try {
+        fs.writeFileSync(cookieFile, totalCookies.toString());
+        console.log(`Saved ${totalCookies} cookies to file`);
+    } catch (err) {
+        console.error("Error saving cookie count:", err);
+    }
+};
 
 io.on("connection", (socket) => {
     playerCount++;
@@ -23,6 +47,8 @@ io.on("connection", (socket) => {
         totalCookies++;
         // Broadcast the new total to all connected clients
         io.emit("cookie-count", totalCookies);
+        // Save the new count to file
+        saveCookieCount();
     });
 
     socket.on("disconnect", () => {
